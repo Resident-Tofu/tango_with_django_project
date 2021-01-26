74f64f99dc6c281ee1afb8bf9baab212f6182276
Darren Fielding <2424957f@student.gla.ac.uk>
2021-01-26 14:55:17 +0000

    Completed chapter 9 tests.

--- rango/views.py
+++ rango/views.py
@@ -7,7 +7,8 @@ from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # How to replace URLs using `reverse()`?
 
@@ -38,6 +39,7 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context = context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -52,6 +54,7 @@ def add_category(request):
 
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug = category_name_slug)
@@ -131,3 +134,12 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+    return render(request, 'rango/restricted.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
